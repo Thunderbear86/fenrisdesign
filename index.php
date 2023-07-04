2f4b3021b6963ecdbdb785c9190e2eefc24606c0
Thorbjørn <benj434d@edu.zealand.dk>
2023-07-04 14:48:58 +0200

    Landingpage udkast 1

--- index.php
+++ index.php
@@ -16,7 +16,141 @@
 
 <body>
 
+<!-- Cover image -->
 
+<div class="container-fluid">
+    <div class="row">
+        <img src="#" alt="Cover Image">
+    </div>
+</div>
+
+<!-- newest / popuplar vertical posters -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- custom poster banner -->
+
+<div class="container-fluid">
+    <div class="row">
+        <img src="#" alt="Lav selv plakat">
+    </div>
+</div>
+
+
+<!-- newest / popular square poster -->
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<!-- newest popular vertical posters -->
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <div class="card">
+                <img src="path/to/image.jpg" class="card-img-top" alt="Image">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some example text to go below the image.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- Footer -->
+
+<div class="container-fluid">
+    <div class="row"></div>
+</div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
